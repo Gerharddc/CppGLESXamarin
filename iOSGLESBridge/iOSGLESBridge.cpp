4baba980d4f8c45fbfc108683250b4e075b6bb6b
Gerhard de Clercq <gerhard@Gerhards-Mac-Pro.local>
2015-06-20 23:15:22 -0700

    Add iOS projects

---
+++ iOSGLESBridge/iOSGLESBridge.cpp
@@ -0,0 +1,9 @@
+//
+//  iOSGLESBridge.cpp
+//  iOSGLESBridge
+//
+//  Created by Gerhard de Clercq on 6/20/15.
+//  Copyright (c) 2015 MyCompany. All rights reserved.
+//
+
+#include "iOSGLESBridge.h"
---
+++ iOSGLESBridge/iOSGLESBridge.h
@@ -0,0 +1,14 @@
+//
+//  iOSGLESBridge.h
+//  iOSGLESBridge
+//
+//  Created by Gerhard de Clercq on 6/20/15.
+//  Copyright (c) 2015 MyCompany. All rights reserved.
+//
+
+#ifndef __iOSGLESBridge__iOSGLESBridge__
+#define __iOSGLESBridge__iOSGLESBridge__
+
+#include <stdio.h>
+
+#endif /* defined(__iOSGLESBridge__iOSGLESBridge__) */
